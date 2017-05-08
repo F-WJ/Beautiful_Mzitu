a64e25c6ef300dc1edb560cbdb78d90087614719
F-WJ <364580936@qq.com>
2017-05-08 15:12:47 +0800

    使用requests.get()

--- Mzitu.py
+++ Mzitu.py
@@ -11,8 +11,9 @@ headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 
                          "Chrome/22.0.1207.1 Safari/537.1"}
 # 开始的url地址
 all_url = 'http://www.mzitu.com/all'
+# 使用requests中get方法
 start_html = requests.get(all_url, headers=headers)
-# 请注意，concent 是二进制的数据，一般用于下载图片、视频、音频、等多媒体内容是才使用concent, 对于打印网页内容请使用text
+# 请注意,concent是二进制的数据,一般用于下载图片,视频,音频,等多媒体内容是才使用concent,对于打印网页内容请使用text
 print(start_html.text)
 
 
